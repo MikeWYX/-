29f89e6688f5a494672be60e8cfb3931563c33c6
MikeWYX <109967418+MikeWYX@users.noreply.github.com>
2025-02-14 19:43:50 +0800

    Add files via upload

---
+++ oslab5_PB21020651_武宇星/lab5_framework/lab5_framework/src/myOS/include/string.h
@@ -0,0 +1,7 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+int mystrcmp(unsigned char *str1, unsigned char *str2);
+int mystrncpy(unsigned char *src, unsigned char *dest, unsigned int n);
+
+#endif
